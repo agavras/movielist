b81f65535065fc94e6d7b84bceb51f6b81614ad4
agavras <math.falze@gmail.com>
2022-04-11 14:05:45 +0200

    movielist

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,146 +4,134 @@ const modal = document.getElementById("add-modal");
 const title = document.getElementById("title");
 const img = document.getElementById("image-url");
 const rating = document.getElementById("rating");
-const entryText = document.getElementById("entry-text");
-const main = document.querySelector("body");
 const DeleteDiv = document.getElementById("delete-modal");
 const btnPassive = document.getElementById("no");
 const btnDanger = document.getElementById("yes");
+const btnAdd = document.getElementById("add");
+const btnCancel = document.getElementById("cancel");
+const btnAddMovie = document.getElementById("addmovie");
+const ulHtml = document.getElementById("movie-list");
 
 // declaration et initialisation des variables (global)
 let section = [];
 let sectionID = 0;
+let idToDelete = 0;
 
 // addEventListener
-btnPassive.addEventListener("click", functionNo);//HideDelete(false)
-btnDanger.addEventListener("click", functionYes);//HideDelete(true)
+btnAddMovie.addEventListener("click", OpenFormulaire);
+btnAdd.addEventListener("click", AddFormulaire);
+btnCancel.addEventListener("click", CancelFormulaire);
+btnPassive.addEventListener("click", FunctionNo);
+btnDanger.addEventListener("click", FunctionYes);
 backdrop.addEventListener("click", HideFormulaire);
 
-
-// Cette fonction sera appelée si on clic sur le boutton HTML
+// Cette fonction sera appelée si on clic sur le boutton ADD MOVIE du HTML
 function OpenFormulaire() {
     backdrop.style.display = "block";
     modal.style.display = "block";
     console.log("formulaire ouvert");
 }
 
+// Cette fonction sera appelée si on clic sur le boutton CANCEL du Formulaire ADD MOVIE
+function CancelFormulaire() {
+    console.log("formulaire annulé");
+    HideFormulaire();
+
+}
+
+// Cette fonction sera appelée pour cacher le formulaire
 function HideFormulaire() {
     backdrop.style.display = "none";
     modal.style.display = "none";
-    console.log("formulaire caché ou annulé");
+    console.log("formulaire caché");
 }
+
+// Cette fonction sera appelée si on clic sur le boutton ADD du Formulaire ADD MOVIE
 function AddFormulaire() {
     let NewTitle = title.value;
     let NewImg = img.value;
     let NewRating = rating.value;
 
     if (NewTitle !== "") {
-    console.log("title valide");
+    console.log("titre valide");
         if (NewImg !== "") {
         console.log("image valide");
             if (NewRating !== "" && NewRating < 6 && NewRating > 0) {
-            console.log("rating valide");
-            } else {   
-            console.log("rating vide");
-            alert("Please enter valid RATING (1 to 5)");
+            console.log("note valide");
+            } else {  
+            NewRating=null;
+            console.log("note invalide");
+            alert("Entrez une NOTE valide (1 to 5)");
             }
         } else {
-        console.log("image vide");
-        alert("Please enter valid URL");
+        NewImg=null;
+        console.log("image invalide");
+        alert("Entrez une URL valide");
         }
     } else {
-    console.log("title vide");
-    alert("Please enter valid TITLE");
+    NewTitle=null;
+    console.log("titre invalide");
+    alert("Entrez un TITRE valide");
     }
-    console.log("formulaire ajouté");
-
+    
     if (NewTitle && NewImg && NewRating) {
-    HideFormulaire();
     add_li();
+    console.log("formulaire ajouté");
+    HideFormulaire();
     }
 }
 
+// Cette fonction sera appelée si à cliqué sur le boutton ADD du HTML et que les 3 conditions sont remplies
 function add_li() {
     let NewTitle = title.value;
     let NewImg = img.value;
     let NewRating = rating.value;
 
         // on cré une nouvelle section
-        section[sectionID] = document.createElement("section");
+        section[sectionID] = document.createElement("li");
         section[sectionID].setAttribute("class","movie-element");
-        section[sectionID].setAttribute("id", "entry-text");
-        main.appendChild(section[sectionID]);
+        // section[sectionID].setAttribute("id", "entry-text");
+        ulHtml.appendChild(section[sectionID]);
         section[sectionID].addEventListener("click", AskForDelete);
+        section[sectionID].setAttribute("liID", sectionID.toString());
 
         // on cré une nouvelle image
         let oImg = document.createElement("img");
-        oImg.setAttribute("class","movie-element__image");
+        // oImg.setAttribute("class","movie-element__image");
         oImg.src = "./assets/img.jpg";
         section[sectionID].appendChild(oImg);
 
         // on cré un titre h2
-        let h2G = document.createElement("h2"); // on cré un paraG
+        let h2G = document.createElement("h2");
         h2G.innerHTML = NewTitle;
         section[sectionID].appendChild(h2G);
 
         // on cré un btn
-        let pRating = document.createElement("p"); // on cré un ul
-        // pRating.setAttribute("class", "movie-element p")
+        let pRating = document.createElement("p");
         pRating.innerHTML = NewRating;
         section[sectionID].appendChild(pRating);
 
-        //let oUl = document.getElementById("movie-list"); // récupération de la liste
-        // let iLength = oUl.getElementsByTagName("li").length; // longueur de la liste (nombre d'items)
-
-        // let oLi = document.createElement("li"); // on cré un nouveau noeud item de liste
-
-        // // let oText = document.createTextNode("Ceci est l'item de liste n°" + (iLength + 1)); // on cré un noeud texte
-        // let oText = document.createTextNode(NewTitle); // on cré un noeud texte
-        // let oText2 = document.createTextNode(NewImg); // on cré un noeud texte
-        // let oText3 = document.createTextNode(NewRating); // on cré un noeud texte
-
-        // oLi.appendChild(oImg); // on attache le noeud texte au noeud item de liste
-        // oLi.appendChild(oText); // on attache le noeud texte au noeud item de liste
-        // oLi.appendChild(oText2); // on attache le noeud texte au noeud item de liste
-        // oLi.appendChild(oText3); // on attache le noeud texte au noeud item de liste
-        // oUl.appendChild(oLi); // on attache le noeud item de liste au noeud liste
-
-        // // oText.setAttribute("class","movie-element__info");
-
-        // // return oLi;
-        // entryText.appendChild(oUl);
         sectionID ++;
-        console.log(sectionID);
+        // console.log(sectionID);
     }
 
+    // si on clique sur une fiche que l'on à precedament créé
     function AskForDelete() {
-        DeleteDiv.setAttribute("class","modal.visible");
+        DeleteDiv.style.display="block";
+        idToDelete = this.getAttribute("liID")
+        // console.log(idToDelete);
     }
 
-    // function HideDelete(param) {
-    //     let param2 = param;
-    //     // let section = this;
-    //     if (param2 === true) {
-    //         // console.log("true");
-    //         DeleteDiv.setAttribute("class","modal");
-    //         console.log(sectionID-1);
-    //         // section[sectionID].remove();
-
-    //     } else if (param2 === false) {
-    //         // console.log("false");
-    //         DeleteDiv.setAttribute("class","modal");
-    //     }
-    // }
-
-    function functionYes() {
-        let id = sectionID-1;
-        console.log("Yes");
-            DeleteDiv.setAttribute("class","modal");
-            console.log(id);
-            section[id].remove();
+    // si on clique sur YES pour valider la suppression
+    function FunctionYes() {
+            console.log("YES");
+            DeleteDiv.style.display="none";
+            // console.log(idToDelete);
+            section[idToDelete].remove();
     }
     
-    function functionNo() {
-            // console.log("false");
-            DeleteDiv.setAttribute("class","modal");
+    // si on clique sur NO(cancel) pour annuler la suppression
+    function FunctionNo() {
+            console.log("NO");
+            DeleteDiv.style.display="none";
     }
